b034e07d999124e7b0a0ebc12ec2cb0a089cb68e
qiyitao <13255238145@163.com>
2021-08-19 14:47:04 +0800

    实现简单的多聊天室

--- server/index.js
+++ server/index.js
@@ -2,13 +2,22 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 3000 });
 
 let num = 0;
+let group ={}
+// 多聊天室功能
+// 对相同的roomid进行广播
 wss.on("connection", function connection(ws) {
   // 接收客户端的消息
   ws.on("message", function (msg) {
     const msgObj = JSON.parse(msg);
     if (msgObj.event === "enter") {
       ws.name = msgObj.message;
+      ws.roomid = msgObj.roomid;
       num++;
+      if(typeof group[ws.roomid] === undefined){
+        group[ws.roomid]=1
+      }else{
+        group[ws.roomid]++
+      }
     }
     // ws.send(msg)
     // 广播消息
@@ -18,9 +27,10 @@ wss.on("connection", function connection(ws) {
       //   msgObj.num = wss.clients.size
       //   client.send(JSON.stringify(msgObj))
       // }
-      if (client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN&& client.roomid ===ws.roomid) {
         // 为了获取在线的聊天人数
-        msgObj.num = num;
+        msgObj.num = group[ws.roomid];
+        msgObj.name = ws.name
         client.send(JSON.stringify(msgObj));
       }
     });
@@ -31,14 +41,14 @@ wss.on("connection", function connection(ws) {
   // 当ws客户端断开
   ws.on("close", function () {
     if(ws.name){
-      num--;
+      group[ws.roomid]--;
     }
     let msgObj = {}
     // 广播消息
     wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN&& client.roomid ===ws.roomid) {
         // 为了获取在线的聊天人数
-        msgObj.num = num;
+        msgObj.num = group[ws.roomid];
         msgObj.name = ws.name;
         msgObj.event = 'out'
         client.send(JSON.stringify(msgObj));
